909adb98e2f42ee49665c1b8faad1f8ad4ac74db
dreamingblackcat <dreamingblackcat10@gmail.com>
2016-10-18 18:51:44 +0630

    A quick n dirty ruby script to patch new 'rates' structure

---
+++ rates_db_patch.rb
@@ -0,0 +1,24 @@
+require 'rethinkdb'
+include RethinkDB::Shortcuts
+r.connect(:host=>"localhost", :port=>28015).repl
+cursor = r.db("test").table("currency").run
+cursor.each do|d|
+ p d
+ orig_rates = d['rates']
+ next if orig_rates[0]["USD"].nil? #hack for mixed tables
+ new_rates  = []
+ orig_rates.each do|rate|
+     p rate
+     rate.each do|k,v|
+       h = v.clone
+       h['currency_shortcode'] = k
+       p h
+       p v
+       new_rates.push h
+     end
+ end
+ p new_rates 
+ r.db("test_cloned").table("currency_sample").update({rates: new_rates}).run
+ p "======="
+end
+
