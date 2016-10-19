0f4178ba45d9b4da164f389ffa79a85ebb486131
dreamingblackcat <dreamingblackcat10@gmail.com>
2016-10-19 11:44:52 +0630

    Fix db name in ruby script and clean up some logs

--- rates_db_patch.rb
+++ rates_db_patch.rb
@@ -2,22 +2,22 @@ require 'rethinkdb'
 include RethinkDB::Shortcuts
 r.connect(host: 'localhost', port: 28_015).repl
 cursor = r.db('test').table('currency').run
+count = 0
 cursor.each do |d|
-  p d
+  p "Updating document #{d['id']}"
   orig_rates = d['rates']
   next if orig_rates[0]['USD'].nil? # HACK: for mixed tables
   new_rates = []
   orig_rates.each do |rate|
-    p rate
     rate.each do |k, v|
       h = v.clone
       h['currency_shortcode'] = k
-      p h
-      p v
       new_rates.push h
     end
   end
   p new_rates
-  r.db('test_cloned').table('currency_sample').update(rates: new_rates).run
+  r.db('test').table('currency').update(rates: new_rates).run
+  count += 1
   p '======='
 end
+puts "Total #{count} documents updated"
